ce5012a3af378c9507ef1167bcc99c6734b4b309
SHINHWA PERU <gruposhuancayo@gmail.com>
2026-05-12 19:28:25 -0500

    fix standalone railway

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
-const withPWA = require("next-pwa")({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
-});
+import type { NextConfig } from "next";
 
-module.exports = withPWA({
-  reactStrictMode: true,
-  turbopack: {},
-});
+const nextConfig: NextConfig = {
+  output: "standalone",
+};
+
+export default nextConfig;
